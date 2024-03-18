4f852da90a063f3e8512471271be2dcd60cff1e6
kxs1119 <lifeisastage101@outlook.com>
2024-03-17 21:35:00 -0400

    Initial Commit- Plan

---
+++ part1.py
@@ -0,0 +1,26 @@
+# Prompt the user to enter first & last name 
+# Prompt the user to enter an ID 
+# Validate the ID
+# Generate 10 questions from the testbank file 
+# Display 1 question at a time
+# Recieve input of answer choice 
+# If invalid answer, prompt to re enter a valid answer
+''' When quiz is done, create a text file of:
+    Name of file: StudentID_Firstname_Lastname
+    Student ID, First name and Last Name
+    Score
+    Elapsed time
+    Selected questions text, and correct answers,
+    and student answer'''
+    
+''' Prompt users to Q (exit), S(clear&start again)'''
+
+
+''' Creating either a functions or class for 
+    the questions portion:
+    
+    Get Question Function 
+    
+    Set Questions and Answers into a Dictionary 
+    
+    '''
