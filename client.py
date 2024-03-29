3afe7a792c92a5df7f5a3ee4688857900e65def4
Luke Buczynski <lukebuczynski13@gmail.com>
2024-03-29 15:19:44 -0400

    refactor framework

---
+++ client.py
@@ -0,0 +1,13 @@
+import source
+
+
+def main():
+    print("Let it begin")
+    
+    firstName = " "
+    lastName = " "
+    letterId = " "
+    numId = " "
+    
+if __name__ == "__main__":
+    main()
---
+++ source.py
@@ -0,0 +1,20 @@
+import csv
+import random
+
+FILENAME = "testbank.csv"
+quiz = {} # type: ignore
+questions = {}
+
+def validateId():
+    pass
+
+def quiz():
+    #Read in random questions into questions dictionary, add questions dictionary in quiz dictionary
+    pass
+
+def displayQuiz():
+    pass
+
+def createStudentFile():
+    pass
+
