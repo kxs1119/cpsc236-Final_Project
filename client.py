ff45f7851f0624f969aea1efb3d797825a293f85
kxs1119 <lifeisastage101@outlook.com>
2024-04-03 17:52:52 -0400

    completed calculate, display, validate functions, and edit the client code.

--- client.py
+++ client.py
@@ -8,6 +8,36 @@ def main():
     lastName = " "
     letterId = " "
     numId = " "
+    maxIdAttempts = 3
     
+    while True:
+        print("Please enter your first name:")
+        firstName = input()
+        
+        print("Please enter your last name:")
+        lastName = input()
+        
+        print("Please enter your letter id:")
+        print("A valid ID must provide 'A' and 6 numbers")
+        letterId = input()
+        # validating the letter id and if incorrect it will ask the user to try again with a max attempt of 3 times
+        if source.validateId(letterId) == False:
+            print("Please try again")
+            maxIdAttempts -= 1
+            if maxIdAttempts == 0:
+                print("You have exceeded the maximum number of attempts")
+                break
+        
+        
+        complete_test = input("Would you like to complete the test? (Q/S)").lower()
+        # adding redo or quit conditions
+        if complete_test == 'q':
+            print("Thank you for using the test!")
+            break
+        elif complete_test =='s':
+            print("We will now begin the another test!")
+        else:
+            print("Please enter either Q or S")
+        
 if __name__ == "__main__":
     main()
--- source.py
+++ source.py
@@ -1,20 +1,59 @@
 import csv
 import random
+import time # import time module for elapsing time in quiz
 
 FILENAME = "testbank.csv"
 quiz = {} # type: ignore
 questions = {}
 
-def validateId():
-    pass
+def validateId(letterId):
+    # this function should validate the id
+    # it will make sure the first letter is 'A' and the number of digits is 6
+    if len(letterId)!= 6 or letterId[0] != 'A': # type: ignore
+        return False
+    for element in letterId[1:]:
+        if not element.isdigit(): # type: ignore
+            return False
+    return True
+    
 
 def quiz():
     #Read in random questions into questions dictionary, add questions dictionary in quiz dictionary
     pass
 
 def displayQuiz():
+    # this function should display time elapsed and the current question
+    # this function will take from quiz, display a unique questions one at a time from the quiz dictionary
+    # it will also show the options for each question 
+    # it will ask the user for useranswer, store the useranswer in the quiz dictionary
+   
+    start = time.time()
+    question_index = 1
+    while question_index in quiz and time.time() - start < 600: # type: ignore
+        print(f"Question {question_index}: {quiz[question_index]}") # type: ignore
+        print(f"Options: {quiz[question_index]}") # type: ignore
+        userAnswer = input("Enter your answer: ")
+        quiz[question_index] = userAnswer # type: ignore
+        question_index += 1
+        print(f"Time elapsed: {time.time() - start:.2f} seconds") # type: ignore
+        
+    return userAnswer # type: ignore
+def calculateScore(userAnswer, correctAnswer):
+    # this function should calculate the score of the student based on the quiz dictionary
+    # it will compare the student answers to the correct answers in the quiz dictionary
+    # then add 1 to a counter based on the number of correct answers
+    score = 0
+    for question_index in quiz: # type: ignore
+        if quiz[question_index][correctAnswer] == quiz[question_index][userAnswer]: # type: ignore
+            score += 1
+        
+    return score # type: ignore
+    
     pass
 
 def createStudentFile():
+    # this function should create a new student file with the following columns:
+    # first and last name, student id, score, elapsed time, answers {correct answers , student answers}
+    
     pass
 
